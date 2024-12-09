08d842f25c9c9a63427d0708eb54a768e079e026
EslamKamel89 <elsamkamelforex@gmail.com>
2024-12-09 19:27:35 +0200

    creating logout button

--- lib/features/doctor/doctor_home_view.dart
+++ lib/features/doctor/doctor_home_view.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:locum_app/core/router/app_routes_names.dart';
+import 'package:locum_app/core/service_locator/service_locator.dart';
+import 'package:locum_app/utils/styles/styles.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class DoctorHomeScreen extends StatelessWidget {
   const DoctorHomeScreen({super.key});
@@ -8,6 +12,14 @@ class DoctorHomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Doctor Home View'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+            onPressed: () async {
+              await serviceLocator<SharedPreferences>().clear();
+              Navigator.of(context).pushNamedAndRemoveUntil(AppRoutesNames.signinScreen, (_) => false);
+            },
+            child: txt('Log out')),
       ),
     );
   }
--- lib/features/hospital/hospital_home_view.dart
+++ lib/features/hospital/hospital_home_view.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:locum_app/core/router/app_routes_names.dart';
+import 'package:locum_app/core/service_locator/service_locator.dart';
+import 'package:locum_app/utils/styles/styles.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HospitalHomeScreen extends StatelessWidget {
   const HospitalHomeScreen({super.key});
@@ -8,6 +12,14 @@ class HospitalHomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Hospital Home View'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+            onPressed: () async {
+              await serviceLocator<SharedPreferences>().clear();
+              Navigator.of(context).pushNamedAndRemoveUntil(AppRoutesNames.signinScreen, (_) => false);
+            },
+            child: txt('Log out')),
       ),
     );
   }
